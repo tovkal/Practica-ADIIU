c75fc77026545aa6c49e1a4f1ff2254ea6846766
tovkal <Tovkal@gmail.com>
2014-12-22 19:24:56 +0100

    Entradas setup func and moved in Salidas

--- ws/entradas_test.go
+++ ws/entradas_test.go
@@ -3,27 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 )
 
 var originalEntrada = Entradas{Id: 1, Idmedicamento: 1, Cantidad: 22}
 var testEntrada = Entradas{Idmedicamento: 1, Cantidad: 666}
-
-func TestMain(m *testing.M) {
-
-	fechaHora, err := time.Parse("2006-01-02 15:04:05", "2014-11-26 16:18:16")
-	if err != nil {
-		fmt.Println("Error %s", err)
-	}
-	originalEntrada.Fechahora = fechaHora
-
-	fechaHora, _ = time.Parse("2006-01-02 15:04:05", "2014-12-12 12:12:12")
-	testEntrada.Fechahora = fechaHora
-
-	os.Exit(m.Run())
-}
 
 func TestGetAllEntradas(t *testing.T) {
 	resp := test("entradas", "GET", "")
@@ -60,4 +45,15 @@ func isReturnedEntradaExpected(compareTo Entradas, responseStruct Entradas, t *t
 		b, _ := json.Marshal(compareTo)
 		t.Errorf("Entrada retrieved does not match the expected result. Got \n%s, expected \n%s", a, b)
 	}
+}
+
+func setup() {
+	fechaHora, err := time.Parse("2006-01-02 15:04:05", "2014-11-26 16:18:16")
+	if err != nil {
+		fmt.Println("Error %s", err)
+	}
+	originalEntrada.Fechahora = fechaHora
+
+	fechaHora, _ = time.Parse("2006-01-02 15:04:05", "2014-12-12 12:12:12")
+	testEntrada.Fechahora = fechaHora
 }
--- ws/salidas_test.go
+++ ws/salidas_test.go
@@ -10,17 +10,6 @@ import (
 var originalSalida = Salidas{Id: 3, Idmedicamento: 1, Cantidad: 5, Idfarmacia: 1}
 var testSalida = Salidas{Idmedicamento: 1, Cantidad: 666, Idfarmacia: 1}
 
-func setup() {
-	fechaHora, err := time.Parse("2006-01-02 15:04:05", "2014-11-29 14:10:04")
-	if err != nil {
-		fmt.Println("Error %s", err)
-	}
-	originalSalida.Fechahora = fechaHora
-
-	fechaHora, _ = time.Parse("2006-01-02 15:04:05", "2014-12-12 12:12:12")
-	testSalida.Fechahora = fechaHora
-}
-
 func TestGetAllSalidas(t *testing.T) {
 	resp := test("salidas", "GET", "")
 
@@ -59,4 +48,15 @@ func isReturnedSalidaExpected(compareTo Salidas, responseStruct Salidas, t *test
 		b, _ := json.Marshal(compareTo)
 		t.Errorf("Salida retrieved does not match the expected result. Got \n%s, expected \n%s", a, b)
 	}
+}
+
+func setup() {
+	fechaHora, err := time.Parse("2006-01-02 15:04:05", "2014-11-29 14:10:04")
+	if err != nil {
+		fmt.Println("Error %s", err)
+	}
+	originalSalida.Fechahora = fechaHora
+
+	fechaHora, _ = time.Parse("2006-01-02 15:04:05", "2014-12-12 12:12:12")
+	testSalida.Fechahora = fechaHora
 }
