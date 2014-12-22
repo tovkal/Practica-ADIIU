8caccbc5783fee3461c4f05faf5db0ae21063c61
tovkal <Tovkal@gmail.com>
2014-12-22 18:24:37 +0100

    Changes to Categorias tests

--- ws/categorias_test.go
+++ ws/categorias_test.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strconv"
 	"testing"
 )
 
-var original = Categorias{Id: 1, Nombre: "Pediatría", Texto: "Productos para niños de 0 a 3 años", Imagen: "pediatria.png"}
-var testStruct = Categorias{Nombre: "Nombre", Texto: "Descripción", Imagen: "image.png"}
+var originalCategoria = Categorias{Id: 1, Nombre: "Pediatría", Texto: "Productos para niños de 0 a 3 años", Imagen: "pediatria.png"}
+var testCategoria = Categorias{Nombre: "Nombre", Texto: "Descripción", Imagen: "image.png"}
 
 func TestGetAllCategorias(t *testing.T) {
 	resp := test("categorias", "GET", "")
@@ -26,25 +22,25 @@ func TestGetCategoria(t *testing.T) {
 
 	responseStruct := Categorias{}
 	decodeJsonPayload(resp, &responseStruct, t)
-	checkReturnedExpected(original, responseStruct, t)
+	isReturnedCategoriaExpected(originalCategoria, responseStruct, t)
 }
 
 func TestPostCategoria(t *testing.T) {
-	jsonBytes, _ := json.Marshal(testStruct)
+	jsonBytes, _ := json.Marshal(testCategoria)
 	resp := test("categorias", "POST", string(jsonBytes))
 
 	isOK(resp, t)
 
 	responseStruct := Categorias{}
 	decodeJsonPayload(resp, &responseStruct, t)
-	checkReturnedExpected(testStruct, responseStruct, t)
+	isReturnedCategoriaExpected(testCategoria, responseStruct, t)
 
 	// Save returned struct, which includes the id for later tests
-	testStruct = responseStruct
+	testCategoria = responseStruct
 }
 
 func TestPutCategoria(t *testing.T) {
-	updated := testStruct
+	updated := testCategoria
 	updated.Texto = "Updated"
 	jsonBytes, _ := json.Marshal(updated)
 	resp := test("categorias/"+strconv.FormatInt(updated.Id, 10), "PUT", string(jsonBytes))
@@ -53,56 +49,21 @@ func TestPutCategoria(t *testing.T) {
 
 	responseStruct := Categorias{}
 	decodeJsonPayload(resp, &responseStruct, t)
-	checkReturnedExpected(updated, responseStruct, t)
+	isReturnedCategoriaExpected(updated, responseStruct, t)
 }
 
 func TestDeleteCategoria(t *testing.T) {
-	resp := test("categorias/"+strconv.FormatInt(testStruct.Id, 10), "DELETE", "")
+	resp := test("categorias/"+strconv.FormatInt(testCategoria.Id, 10), "DELETE", "")
 
 	isOK(resp, t)
 }
 
 // Private functions
 
-func test(operation string, method string, json string) (response *http.Response) {
-	url := "http://localhost:8080/" + operation
-	fmt.Println("URL:>", url)
-
-	var jsonStr = []byte(json)
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json; charset=utf8")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	return resp
-}
-
-func isOK(resp *http.Response, t *testing.T) {
-	if resp.StatusCode != http.StatusOK {
-		t.Error("Operation did not return OK, got ", resp.Status)
-	}
-}
-
-func decodeJsonPayload(r *http.Response, v interface{}, t *testing.T) {
-	content, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		t.Error("Error reading body from response.", err)
-	}
-	defer r.Body.Close()
-	err = json.Unmarshal(content, v)
-	if err != nil {
-		t.Error("Error unmarshaling response.", err)
-	}
-}
-
-func checkReturnedExpected(compareTo Categorias, responseStruct Categorias, t *testing.T) {
+func isReturnedCategoriaExpected(compareTo Categorias, responseStruct Categorias, t *testing.T) {
 	if !compareCategorias(compareTo, responseStruct) {
-		a, _ := json.Marshal(compareTo)
-		b, _ := json.Marshal(responseStruct)
+		a, _ := json.Marshal(responseStruct)
+		b, _ := json.Marshal(compareTo)
 		t.Errorf("Categoria retrieved does not match the expected result. Got \n%s, expected \n%s", a, b)
 	}
 }
---
+++ ws/common_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func test(operation string, method string, json string) (response *http.Response) {
+	url := "http://localhost:8080/" + operation
+	fmt.Println("URL:>", url)
+
+	var jsonStr = []byte(json)
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json; charset=utf8")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func isOK(resp *http.Response, t *testing.T) {
+	if resp.StatusCode != http.StatusOK {
+		t.Error("Operation did not return OK, got ", resp.Status)
+	}
+}
+
+func decodeJsonPayload(r *http.Response, v interface{}, t *testing.T) {
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		t.Error("Error reading body from response.", err)
+	}
+	defer r.Body.Close()
+	err = json.Unmarshal(content, v)
+	if err != nil {
+		t.Error("Error unmarshaling response.", err)
+	}
+}
